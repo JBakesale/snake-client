66653ba58a0dc440a4ef3a436790f2b17ce49719
Joshua Baker <joshuadfbaker@gmail.com>
2023-07-05 23:00:16 -0700

    snake finished

--- client.js
+++ client.js
@@ -14,20 +14,9 @@ const connect = function () {
     console.log(data);
   });
 
-  const move = (direction, time) => {
-    setTimeout(() => {
-      conn.write(`Move: ${direction}`);
-    }, time * 1000);
-  };
-
   conn.on("connect", () => {
     console.log("Client connection successful");
     conn.write("Name: JDB");
-    // move("up", 0);
-    // move("up", 1);
-    // move("left", 2);
-    // move("left", 3);
-    // move("down", 4);
   });
 
   return conn;
--- input.js
+++ input.js
@@ -1,11 +1,42 @@
+let connection;
+
+const stdin = process.stdin;
+const stdout = process.stdout;
+
 const handleUserInput = (input) => {
   if (input === "\u0003") {
     process.exit();
   }
+
+  if (input === "\u0077") {
+    setTimeout(() => {
+      connection.write("Move: up");
+    }, 50);
+  }
+
+  if (input === "\u0061") {
+    setTimeout(() => {
+      connection.write("Move: left");
+    }, 50);
+  }
+
+  if (input === "\u0073") {
+    setTimeout(() => {
+      connection.write("Move: down");
+    }, 50);
+  }
+
+  if (input === "\u0064") {
+    setTimeout(() => {
+      connection.write("Move: right");
+    }, 50);
+  }
 };
 
-const setupInput = function () {
-  const stdin = process.stdin;
+
+const setupInput = (conn) => {
+  connection = conn;
+
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
@@ -14,6 +45,5 @@ const setupInput = function () {
 
   return stdin;
 };
-
 
 module.exports = { setupInput };
